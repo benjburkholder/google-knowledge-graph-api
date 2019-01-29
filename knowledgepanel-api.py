88d0769aae5172a71252774448ac2cff6e084ea6
Ben <benjburkholder@gmail.com>
2019-01-29 07:39:23 -0500

    added input feature

--- knowledgepanel-api.py
+++ knowledgepanel-api.py
@@ -4,19 +4,27 @@ import urllib.parse
 import requests
 
 api_key = open('api-key.txt').read()
-query = 'swans'
+query = input('Enter query: ')
 service_url = 'https://kgsearch.googleapis.com/v1/entities:search'
 params = {
-    'query': query,
+    'query': str(query),
     'limit': 10,
     'indent': True,
     'key': api_key,
 }
 url = f'{service_url}?{urllib.parse.urlencode(params)}'
 response = json.loads(urllib.request.urlopen(url).read())
-file = open('knowledge-graph-results.txt', 'a')
-file.write(f'Query: {query}' + '\n')
 for element in response['itemListElement']:
   print(element['result']['name'] + ' (' + str(element['resultScore']) + ')')
-  file.write(element['result']['name'] + ' (' + str(element['resultScore']) + ')' + '\n')
-file.close()
+
+print('')
+savefiles = input('Save files locally?(Y/N) ')
+if savefiles == 'Y':
+  file = open('knowledge-graph-results.txt', 'a')
+  file.write(f'Query: {query}' + '\n')
+  for element in response['itemListElement']:
+    print(element['result']['name'] + ' (' + str(element['resultScore']) + ')')
+    file.write(element['result']['name'] +
+             ' (' + str(element['resultScore']) + ')' + '\n')
+  file.close()
+
