aa383496ebbdf8b6730743dd3e59948163ec6c33
Ben <benjburkholder@gmail.com>
2019-02-18 08:18:39 -0500

    fixed csv write

--- knowledgepanel-api.py
+++ knowledgepanel-api.py
@@ -41,19 +41,35 @@ while True:
     print('')
     savefiles = input('Save files locally?(Y/N) ')
     if savefiles == 'Y':
-        savefile = 'knowledge-graph-results.csv'
-        file = open('knowledge-graph-results.csv', 'a')
-        file.write(f'Query: {query}' + '\n')
+        download_dir = 'knowledge-graph-results.csv'
+        file = open(download_dir, 'w')
+        columnTitleRow = "Query, Name, MID, Score, Description\n"
+        file.write(columnTitleRow)
+            
         try:
             for element in response['itemListElement']:
                 if queryDesc == 'Y':
-                    file.write(element['result']['name'] + ' (ID: ' + element['result']['@id'] + ')' + ' (Score: ' + str(element['resultScore']) + ')' + ' Description: ' + element['result']['detailedDescription']['articleBody'] + '\n')
-                else: 
-                    file.write(element['result']['name'] + ' (ID: ' + element['result']['@id'] + ')' + ' (Score: ' + str(element['resultScore']) + ')' + '\n')
+                    q = query
+                    name = element['result']['name']
+                    ID = element['result']['@id']
+                    score = element['resultScore']
+                    description = element['result']['detailedDescription']['articleBody']
+                    descrip2 = f'"{description}"'
+
+                    row = f'{q},{name},{ID},{score},{descrip2}\n'
+                    file.write(row)
+                else:
+                    q = query
+                    name = element['result']['name']
+                    ID = element['result']['@id']
+                    score = element['resultScore']
+                    description = element['result']['detailedDescription']['articleBody']
+                    row2 = f'{q},{name},{ID},{score}\n'
+                    file.write(row2)
         except KeyError:
             print('')
     if savefiles == 'Y':
-        print(f'Success. Results saved locally at {savefile}.')
+        print(f'Success. Results saved locally at {download_dir}.')
         file.close()
     choice = input('Continue Querying?(Y/N): ')
     if choice == 'N':
